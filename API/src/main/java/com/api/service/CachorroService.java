7fdd6587b926d2b11849cbad94bb4085933ecf71
joasmarquesoliveira <joas.oliveira@germinare.org.br>
2026-03-19 09:00:40 -0300

    Fix: Resolvendo um problema de lógica na função atualizarParcialmente

--- API/src/main/java/com/api/service/CachorroService.java
+++ API/src/main/java/com/api/service/CachorroService.java
@@ -126,47 +126,47 @@ public class CachorroService {
     public CachorroResponseDTO atualizarParcialmente(Long id, CachorroRequestDTO req){
         Cachorro caoExistente = objectMapper.convertValue(repositoryCachorro.findById(id), Cachorro.class);
 
-        if (caoExistente.getNome() != null) {
+        if (req.getNome() != null) {
             caoExistente.setNome(req.getNome());
         }
 
-        if (caoExistente.getDataNascimento() != null) {
+        if (req.getDataNascimento() != null) {
             caoExistente.setDataNascimento(req.getDataNascimento());
         }
 
-        if (caoExistente.getTurma() != null) {
+        if (req.getTurma() != null) {
             caoExistente.setTurma(req.getTurma());
         }
 
-        if (caoExistente.getSexo() != null) {
+        if (req.getSexo() != null) {
             caoExistente.setSexo(req.getSexo());
         }
 
-        if (caoExistente.getRaca() != null) {
+        if (req.getRaca() != null) {
             caoExistente.setRaca(req.getRaca());
         }
 
-        if (caoExistente.getAtivo() != null) {
+        if (req.getAtivo() != null) {
             caoExistente.setAtivo(req.getAtivo());
         }
 
-        if (caoExistente.getTemPedigree() != null) {
+        if (req.getTemPedigree() != null) {
             caoExistente.setTemPedigree(req.getTemPedigree());
         }
 
-        if (caoExistente.getSinPatinhas() != null) {
+        if (req.getSinPatinhas() != null) {
             caoExistente.setSinPatinhas(req.getSinPatinhas());
         }
 
-        if (caoExistente.getAceito() != null) {
+        if (req.getAceito() != null) {
             caoExistente.setAceito(req.getAceito());
         }
 
-        if (caoExistente.getImagem() != null) {
+        if (req.getImagem() != null) {
             caoExistente.setImagem(req.getImagem());
         }
 
-        if (caoExistente.getAlergias() != null) {
+        if (req.getAlergias() != null) {
             caoExistente.setAlergia(req.getAlergias());
         }
 
